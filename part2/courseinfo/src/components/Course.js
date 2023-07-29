8934eac3250a9176f2ff775abc707e68271fc62f
Ajay Dangi <ajay_dangi@persistent.com>
2023-07-29 23:17:33 +0530

    Added new files

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,10 +1,12 @@
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = (props)=>(
     <>
         <Header name={props.course.name} />
         <Content parts={props.course.parts} />
+        <Total parts={props.course.parts} />
     </>
 )
 
---
+++ part2/courseinfo/src/components/Total.js
@@ -0,0 +1,13 @@
+const Total = ({parts})=>{
+
+    let sum = 0
+    parts.forEach(part => {
+        sum += part.exercises
+    });
+
+    return(
+        <p>Number of exercises {sum}</p>
+    )
+}
+
+export default Total
